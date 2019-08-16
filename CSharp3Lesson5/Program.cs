3d04bbae16367c2face15456f3a44985f4a4dc6f
Alexandr <alex3289@gmail.com>
2019-08-16 16:40:59 +0600

    Добавлен вывод суммы и проверка на 0

--- CSharp3Lesson5/Program.cs
+++ CSharp3Lesson5/Program.cs
@@ -20,25 +20,50 @@ namespace CSharp3Lesson5
         static void Main(string[] args)
         {
             Console.WriteLine("Введите число");
-            if (int.TryParse(Console.ReadLine(), out int number))
+            if (int.TryParse(Console.ReadLine(), out int number) && number > 0)
             {
                 NumberEntered numberEntered = new NumberEntered
                 {
                     Number = number
                 };
 
-                Thread thread = new Thread(new ParameterizedThreadStart(Factorial));
-                thread.Start(numberEntered);
+                Thread threadFactorial = new Thread(new ParameterizedThreadStart(Factorial));
+                threadFactorial.Start(numberEntered);
 
+                Thread threadSum = new Thread(new ParameterizedThreadStart(Sum));
+                threadSum.Start(numberEntered);
             }
             else
             {
-                Console.WriteLine("Введено не число");
+                Console.WriteLine("Введено не правильное значение");
             }
 
             Console.ReadKey();
         }
 
+        /// <summary>
+        /// Сумма натуральных чисел
+        /// </summary>
+        /// <param name="objNumb"></param>
+        static void Sum(object objNumb)
+        {
+
+            if (objNumb is NumberEntered numb)
+            {
+                int n = numb.Number;
+                int sum = 1;   // значение суммы
+
+                for (int i = 2; i <= n; i++) // цикл начинаем с 2, т.к. нет смысла начинать с 1
+                {
+                    sum += i;
+                }
+                Console.WriteLine("Сумма чисел: " + sum);
+            }
+        }
+        /// <summary>
+        /// Факториал
+        /// </summary>
+        /// <param name="objNumb"></param>
         static void Factorial(object objNumb)
         {
             
@@ -51,7 +76,7 @@ namespace CSharp3Lesson5
                 {
                     factorial *= i;
                 }
-                Console.WriteLine(factorial);
+                Console.WriteLine("Факториал числа: " + factorial);
             }
         }
     }
